5b400bedae00528c0b7a5b97f9c7003abdcbc98f
ThinkInvisible <ThinkInvis@users.noreply.github.com>
2020-08-17 10:06:49 -0500

    Move shocked status patch to ShockOverride

--- Admiral.cs
+++ Admiral.cs
@@ -8,8 +8,6 @@ using Mono.Cecil.Cil;
 using System;
 using BepInEx.Configuration;
 using R2API;
-using System.Collections.Generic;
-using RoR2.Orbs;
 
 namespace ThinkInvisible.Admiral {
     
@@ -24,7 +22,6 @@ namespace ThinkInvisible.Admiral {
         public static float fireDelayDynamic {get; private set;}
         public static float fireDelayFixed {get; private set;}
 
-        internal static Xoroshiro128Plus shockRng;
 
         public void Awake() {
             ConfigFile cfgFile = new ConfigFile(Paths.ConfigPath + "\\" + ModGuid + ".cfg", true);
@@ -58,43 +55,6 @@ namespace ThinkInvisible.Admiral {
             HealOverride.Patch();
             EquipmentRestockOverride.Patch();
             ShockOverride.Patch();
-
-
-            //Apply shock patch
-            shockRng = new Xoroshiro128Plus(0u);
-            EntityStates.ShockState.healthFractionToForceExit = 1f;
-            On.EntityStates.ShockState.FixedUpdate += On_ShockStateFixedUpdate;
-        }
-
-        private void On_ShockStateFixedUpdate(On.EntityStates.ShockState.orig_FixedUpdate orig, EntityStates.ShockState self) {
-            orig(self);
-            if(shockRng.nextNormalizedFloat < 0.033f) { //works out as roughly 10/sec
-                var teamFilter = self.outer.commonComponents.teamComponent;
-			    List<TeamComponent> teamMembers = new List<TeamComponent>();
-			    bool isFF = FriendlyFireManager.friendlyFireMode != FriendlyFireManager.FriendlyFireMode.Off;
-			    if(isFF || teamFilter.teamIndex == TeamIndex.Monster) teamMembers.AddRange(TeamComponent.GetTeamMembers(TeamIndex.Monster));
-			    if(isFF || teamFilter.teamIndex == TeamIndex.Neutral) teamMembers.AddRange(TeamComponent.GetTeamMembers(TeamIndex.Neutral));
-			    if(isFF || teamFilter.teamIndex == TeamIndex.Player) teamMembers.AddRange(TeamComponent.GetTeamMembers(TeamIndex.Player));
-			    float sqrad = 15f * 15f;
-                var tpos = self.outer.commonComponents.characterBody.transform.position;
-			    teamMembers.Remove(teamFilter);
-                teamMembers.RemoveAll(x => (x.transform.position - tpos).sqrMagnitude > sqrad || !x.body || !x.body.mainHurtBox || !x.body.isActiveAndEnabled);
-                if(teamMembers.Count == 0) return;
-                var victim = shockRng.NextElementUniform(teamMembers);
-				OrbManager.instance.AddOrb(new LightningOrb {
-					bouncesRemaining = 0,
-					damageColorIndex = DamageColorIndex.Default,
-					damageType = DamageType.AOE,
-					damageValue = self.outer.commonComponents.characterBody.maxHealth * 0.02f, // ~= 20% maxhealth/sec total dps
-					isCrit = false,
-					lightningType = LightningOrb.LightningType.Tesla,
-					origin = tpos,
-					procChainMask = default,
-					procCoefficient = 1f,
-					target = victim.body.mainHurtBox,
-					teamIndex = TeamIndex.None
-				});
-            }
         }
 
         private void On_CapChargeShotgunFixedUpdate(On.EntityStates.Captain.Weapon.ChargeCaptainShotgun.orig_FixedUpdate orig, EntityStates.Captain.Weapon.ChargeCaptainShotgun self) {
--- Skills/ShockOverride.cs
+++ Skills/ShockOverride.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using RoR2.Skills;
+using RoR2;
+using System.Collections.Generic;
+using RoR2.Orbs;
 
 namespace ThinkInvisible.Admiral {
     public static class ShockOverride {
+        internal static Xoroshiro128Plus shockRng;
+
         internal static void Patch() {
             var shockSkillDef = Resources.Load<SkillDef>("skilldefs/captainbody/CallSupplyDropShocking");
             shockSkillDef.rechargeStock = 1;
@@ -11,6 +16,42 @@ namespace ThinkInvisible.Admiral {
             var shockPrefab = Resources.Load<GameObject>("prefabs/networkedobjects/captainsupplydrops/CaptainSupplyDrop, Shocking");
             var shockDecayer = shockPrefab.AddComponent<CaptainBeaconDecayer>();
             shockDecayer.lifetime = 8f;
+
+            shockRng = new Xoroshiro128Plus(0u);
+            EntityStates.ShockState.healthFractionToForceExit = 1f;
+            On.EntityStates.ShockState.FixedUpdate += On_ShockStateFixedUpdate;
         }
+
+        private static void On_ShockStateFixedUpdate(On.EntityStates.ShockState.orig_FixedUpdate orig, EntityStates.ShockState self) {
+            orig(self);
+            if(shockRng.nextNormalizedFloat < 0.033f) { //works out as roughly 10/sec
+                var teamFilter = self.outer.commonComponents.teamComponent;
+			    List<TeamComponent> teamMembers = new List<TeamComponent>();
+			    bool isFF = FriendlyFireManager.friendlyFireMode != FriendlyFireManager.FriendlyFireMode.Off;
+			    if(isFF || teamFilter.teamIndex == TeamIndex.Monster) teamMembers.AddRange(TeamComponent.GetTeamMembers(TeamIndex.Monster));
+			    if(isFF || teamFilter.teamIndex == TeamIndex.Neutral) teamMembers.AddRange(TeamComponent.GetTeamMembers(TeamIndex.Neutral));
+			    if(isFF || teamFilter.teamIndex == TeamIndex.Player) teamMembers.AddRange(TeamComponent.GetTeamMembers(TeamIndex.Player));
+			    float sqrad = 15f * 15f;
+                var tpos = self.outer.commonComponents.characterBody.transform.position;
+			    teamMembers.Remove(teamFilter);
+                teamMembers.RemoveAll(x => (x.transform.position - tpos).sqrMagnitude > sqrad || !x.body || !x.body.mainHurtBox || !x.body.isActiveAndEnabled);
+                if(teamMembers.Count == 0) return;
+                var victim = shockRng.NextElementUniform(teamMembers);
+				OrbManager.instance.AddOrb(new LightningOrb {
+					bouncesRemaining = 0,
+					damageColorIndex = DamageColorIndex.Default,
+					damageType = DamageType.AOE,
+					damageValue = self.outer.commonComponents.characterBody.maxHealth * 0.02f, // ~= 20% maxhealth/sec total dps
+					isCrit = false,
+					lightningType = LightningOrb.LightningType.Tesla,
+					origin = tpos,
+					procChainMask = default,
+					procCoefficient = 1f,
+					target = victim.body.mainHurtBox,
+					teamIndex = TeamIndex.None
+				});
+            }
+        }
+
     }
 }
